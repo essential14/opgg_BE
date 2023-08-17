628abed6ea4033ff5e579894c241e46d591787c2
DESKTOP-IG9H57V\icomm <essential196@i-commtech.com>
2023-08-17 17:46:40 +0900

    [ADD] login,join 구현

---
+++ src/main/java/com/site/opgg_be/OpggBeApplication.java
@@ -0,0 +1,13 @@
+package com.site.opgg_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpggBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpggBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/site/opgg_be/OpggBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.opgg_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpggBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
