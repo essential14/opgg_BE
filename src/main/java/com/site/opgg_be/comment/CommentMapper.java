f1f21b98b8f3577f63daf61affdd8d6096818a30
essential14 <essential193@gmail.com>
2023-09-01 17:19:52 +0900

    [ADD] 정렬 로직 추가 (수정 필요) - 대댓글 정렬 안 됨

--- src/main/java/com/site/opgg_be/comment/CommentMapper.java
+++ src/main/java/com/site/opgg_be/comment/CommentMapper.java
@@ -11,26 +11,23 @@ public interface CommentMapper {
     @Options(useGeneratedKeys = true, keyProperty = "cno")
     public int insertComment(CommentEntity commen);
 
-    @Select("SELECT IFNULL(MAX(order_cno), 0) FROM comments WHERE bno = #{bno}")
-    public int getMaxOrder(int bno);
+    @Select("SELECT `order_cno` FROM comments WHERE cno = #{cno}")
+    public int getOrderCno(int cno);
 
-    @Select("SELECT order_cno FROM comments WHERE cno = #{cno}")
-    public int getOrder_cno(int cno);
+    @Update("UPDATE comments SET `order_cno` = `order_cno` + 1 WHERE bno = #{bno} AND `order_cno` >= #{startOrderCno}")
+    public void upOrderCno(int bno, int startOrderCno);
 
-    @Update("UPDATE comments SET order_cno = order_cno + 1 WHERE bno = #{bno} AND order_cno >= #{order_cno}")
-    public int upOrder_cno(CommentEntity comment);
+    @Update("UPDATE comments SET group_cno = #{group_cno} WHERE cno = #{cno}")
+    void updateGroupCno(CommentEntity comment);
+
+    @Select("SELECT * FROM comments WHERE bno = #{bno} ORDER BY group_cno ASC, order_cno ASC;")
+    public List<CommentDTO> getCommenList(int bno);
 
     @Update("")
     public void updateComment(CommentDTO dto);
 
-    @Update("UPDATE comments SET group_cno = #{group_cno} WHERE parent_cno = #{parent_cno}")
-    void updateGroupCno(CommentEntity comment);
-
     @Delete("")
     public int deleteCommnet(CommentDTO dto);
 
-
-    @Select("SELECT * FROM comments WHERE bno = #{bno} ORDER BY group_cno ASC, order_cno ASC, comment_date ASC;")
-    public List<CommentDTO> getCommenList(int bno);
 
 }
--- src/main/java/com/site/opgg_be/comment/CommentServiceImpl.java
+++ src/main/java/com/site/opgg_be/comment/CommentServiceImpl.java
@@ -30,22 +30,28 @@ public class CommentServiceImpl implements CommentService {
     private void handleComment(CommentEntity comment) {
         comment.setDepth(0);
         comment.setParent_cno(0);
-        comment.setOrder_cno(mapper.getMaxOrder(comment.getBno()) + 1); // 최대 order_cno 값을 가져와서 +1
+        comment.setOrder_cno(0); //
         mapper.insertComment(comment);
-        comment.setGroup_cno(comment.getCno());
+        comment.setGroup_cno(comment.getCno()); // group_cno를 새로 생성된 cno로 설정
         mapper.updateGroupCno(comment);
     }
 
+
     private void handleReply(CommentEntity comment) {
         comment.setDepth(comment.getDepth() + 1);
+
+        // 부모 댓글의 바로 다음 order_cno 값을 찾기
+        int nextOrderCno = mapper.getOrderCno(comment.getCno()) + 1;
+
+        // nextOrderCno 값과 동일하거나 큰 order_cno를 가진 모든 댓글의 order_cno 값을 1씩 증가
+        mapper.upOrderCno(comment.getBno(), nextOrderCno);
+
+        comment.setOrder_cno(nextOrderCno);
         comment.setParent_cno(comment.getCno());
         comment.setGroup_cno(comment.getGroup_cno());
-        // 댓글 먼저 삽입
         mapper.insertComment(comment);
-        // 삽입 후 cno 값을 기반으로 order_cno 값을 설정
-        comment.setOrder_cno(mapper.getOrder_cno(comment.getCno()) + 1);
-        mapper.upOrder_cno(comment); // 새로 추가될 대댓글보다 order_cno 값이 큰 모든 댓글의 order 값을 +1
     }
+
 
 
     @Override
